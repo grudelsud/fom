87a76a855474fcaba4af31e0b3065150bd3e5c80
fedefrappi <federico.frappi@gmail.com>
2011-04-12 22:35:08 +0200

    Geo-Clustering algorithm slightly changed to fix the wrong behavior

--- java/fom/src/fom/clustering/algorithms/hierarchical/GreedyHAC.java
+++ java/fom/src/fom/clustering/algorithms/hierarchical/GreedyHAC.java
@@ -21,34 +21,40 @@ public class GreedyHAC implements Clusterer {
 	public List<List<Post>> performClustering(List<Post> data){
 		long startTime = System.currentTimeMillis();
 		List<List<Post>> results = new ArrayList<List<Post>>();
-		boolean change = true;
+		boolean shouldMerge = true;
 		List<HierarchicalPostCluster> clusters = new ArrayList<HierarchicalPostCluster>(data.size());
 		for(Post obj : data){
 			clusters.add(new HierarchicalPostCluster(obj));
 		}
-		while(change){
-			System.out.println("Starting iteration...");
-			long iterStartTime = System.currentTimeMillis();
-			change=false;
+		
+		while(shouldMerge){
+			shouldMerge=false;
+			int toBeMergedFirstIndex = 0;
+			int toBeMergedSecondIndex = 0;
 			for(int i=0; i<clusters.size(); i++){
-				for(int j=0; j<clusters.size(); j++){
-					if(i!=j){
-						double distance = distMeasure.getDistance(clusters.get(i), clusters.get(j));
-						if(distance<limit){
-							clusters.get(i).mergeWith(clusters.get(j));
-							clusters.remove(j);
-							change=true;
-							break;
-						}						
-					}
+				for(int j=i+1; j<clusters.size(); j++){
+					double distance = distMeasure.getDistance(clusters.get(i), clusters.get(j));
+					if(distance<limit){
+						shouldMerge=true;
+						toBeMergedFirstIndex = i;
+						toBeMergedSecondIndex = j;
+						break;
+					}						
+				}
+				if(shouldMerge){
+					break;
 				}
 			}
-			System.out.println("Iteration ended in " + (System.currentTimeMillis()-iterStartTime)/1000 + " sec, " + clusters.size() + " clusters found");
+			if(shouldMerge){
+				clusters.get(toBeMergedFirstIndex).mergeWith(clusters.get(toBeMergedSecondIndex));
+				clusters.remove(toBeMergedSecondIndex);
+			}
 		}
+		
 		for(HierarchicalPostCluster cluster : clusters){
 			results.add(cluster.getObjects());
 		}
-		System.out.println("Clustering completed in " + (System.currentTimeMillis()-startTime)/1000 + " sec");
+		System.out.println("Clustering completed in " + (System.currentTimeMillis()-startTime)/1000 + " sec, found " + clusters.size() + "clusters");
 		return results;
 	}
 }
